2dd07ca386d25e1760e01f87e139ab98e52b098d
asjg <alexander.geiger@rwth-aachen.de>
2018-12-09 23:51:15 +0100

    don\'t even know anymore, what I did here

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-ln -s /home/alex/vim-setup/.vimrc /home/alex/.vimrc
+ln -s $HOME/vim-setup/.vimrc $HOME/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
